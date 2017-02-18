f9899650257d7d8b4daf4758f943cc4ce429cf4b
Amitai Schleier <schmonz-web-git@schmonz.com>
2017-02-18 17:30:35 -0500

    Game over!

--- led.c
+++ led.c
@@ -1,8 +1,16 @@
+#include <string.h>
+#include <err.h>
+
 #include "led.h"
 
 #include "syscalls.h"
 
 void toggle_led(void)
 {
-    open("/dev/gpio0", O_WRONLY);
+    struct gpio_req request;
+    memset(&request, 0, sizeof(struct gpio_req));
+    request.gp_pin = 16;
+	int fd = open("/dev/gpio0", O_RDWR);
+    ioctl(fd, GPIOTOGGLE, &request);
+    close(fd);
 }
---
+++ rpi_syscalls.h
@@ -0,0 +1,4 @@
+#include <sys/gpio.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <unistd.h>
